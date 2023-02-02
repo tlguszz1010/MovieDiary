fa9dd3ea5863ca92cfde8ce8c6e9120394d8096c
tlguszz1010 <tlguszz1010@naver.com>
2023-02-02 16:50:22 +0900

    getHomeAPI -> Rx 코드로 변경

--- MovieDiary/Manager/APIManager.swift
+++ MovieDiary/Manager/APIManager.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Alamofire
+import RxSwift
 
 //MARK: 서버에서 받아온 데이터를 Struct 형태로 만들어서 전달. -> Service가 받게됨
 
@@ -14,24 +15,37 @@ import Alamofire
 class HomeAPIManager {
     static let shared = HomeAPIManager()
     // Result 타입 적용
-    func getHomeAPI(url: String, completion: @escaping (Result<ResponseData, Error>) -> Void) {
-        let headers : HTTPHeaders = ["Content-Type" : "application/json;charset=utf-8"]
-        AF.request(url, method: .get, headers: headers).validate().validate(statusCode: 200...500).responseDecodable(of: ResponseData.self ) { response in
-            switch response.result {
-            case let .success(value):
-                guard let statusCode = response.response?.statusCode else { return }
-                print("Success\(statusCode)")
-                completion(.success(value))
-                
-            case let .failure(error):
-                guard let statusCode = response.response?.statusCode else { return }
-                completion(.failure(error))
-                print("Failure\(statusCode)")
+//    func getHomeAPI(url: String, completion: @escaping (Result<ResponseData, Error>) -> Void) {
+//        let headers : HTTPHeaders = ["Content-Type" : "application/json;charset=utf-8"]
+//        AF.request(url, method: .get, headers: headers).validate().validate(statusCode: 200...500).responseDecodable(of: ResponseData.self ) { response in
+//            switch response.result {
+//            case let .success(value):
+//                guard let statusCode = response.response?.statusCode else { return }
+//                print("Success\(statusCode)")
+//                completion(.success(value))
+//                
+//            case let .failure(error):
+//                guard let statusCode = response.response?.statusCode else { return }
+//                completion(.failure(error))
+//                print("Failure\(statusCode)")
+//            }
+//        }
+//    }
+    
+    let headers : HTTPHeaders = ["Content-Type" : "application/json;charset=utf-8"]
+    
+    func getHomeAPIWithRx(url: String) -> Observable<ResponseData> {
+        return Observable.create { emitter in
+            AF.request(url, method: .get, headers: self.headers).validate().validate(statusCode: 200...500).responseDecodable(of: ResponseData.self ) { response in
+                switch response.result {
+                case let .success(value):
+                    emitter.onNext(value)
+                    emitter.onCompleted()
+                case let .failure(error):
+                    emitter.onError(error)
+                }
             }
+            return Disposables.create()
         }
     }
-    
-    
-    // RX 리팩토링
-    
 }
--- MovieDiary/ViewModel/HomeCellViewModel.swift
+++ MovieDiary/ViewModel/HomeCellViewModel.swift
@@ -6,11 +6,12 @@
 //
 
 import Foundation
+import RxSwift
 
 class cellViewModel: BaseViewModel {
     // View로부터 받은 요청
     struct Input {
-        let initTrigger: Dynamic<String?> = Dynamic(nil)
+        let initTrigger: Observable<String> = Observable<String>.just("")
     }
     
     
@@ -23,6 +24,7 @@ class cellViewModel: BaseViewModel {
     var input: Input
     var output: Output
     var posterList: [String] = []
+    let disposeBag = DisposeBag()
     
     init(input: Input = Input(), output: Output = Output()) {
         self.input = input
@@ -32,21 +34,33 @@ class cellViewModel: BaseViewModel {
     
     // View에서 input값이 바뀌었을 때 감지하는 메서드
     private func inputBinding() {
-        self.input.initTrigger.bind { [weak self] url in
-            guard let url = url else { return }
-            HomeAPIManager.shared.getHomeAPI(url: url) { completion in
-                switch completion {
-                case let .success(responseData):
-                    // output의 값에 responseData를 넣어줌으로써 output 값 변경 -> View에서 감지
-                    self?.output.data.value = responseData
-                    for ele in responseData.results {
-                        self?.posterList.append(ele.posterPath)
-                    }
-                case let .failure(error):
-                    print(error.localizedDescription)
-                }
-            }
-        }
+        self.input.initTrigger
+            .subscribe(onNext: { data in
+                
+            }, onError: { error in
+                
+            }, onCompleted: {
+                
+            }, onDisposed: {
+                
+            })
+            .disposed(by: disposeBag)
+        
+//        self.input.initTrigger.bind { [weak self] url in
+//            guard let url = url else { return }
+//            HomeAPIManager.shared.getHomeAPI(url: url) { completion in
+//                switch completion {
+//                case let .success(responseData):
+//                    // output의 값에 responseData를 넣어줌으로써 output 값 변경 -> View에서 감지
+//                    self?.output.data.value = responseData
+//                    for ele in responseData.results {
+//                        self?.posterList.append(ele.posterPath)
+//                    }
+//                case let .failure(error):
+//                    print(error.localizedDescription)
+//                }
+//            }
+//        }
     }
     
     
