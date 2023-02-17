655b7ee2ab75ea2883a966832b9c4200a1129aa7
tlguszz1010 <tlguszz1010@naver.com>
2023-02-17 18:35:29 +0900

    CastCollectionViewCell CastImageView 생성

--- MovieDiary/Search/View/CastCollectionViewCell.swift
+++ MovieDiary/Search/View/CastCollectionViewCell.swift
@@ -6,7 +6,16 @@
 //
 
 import UIKit
+import SnapKit
 
 class CastCollectionViewCell: UICollectionViewCell {
     static let identifier = "CastCollectionViewCell"
+    
+    let castImage: UIImageView = {
+        let image = UIImageView()
+        image.layer.cornerRadius = 10
+        image.layer.borderColor = UIColor.black.cgColor
+        image.layer.borderWidth = 1
+        return image
+    }()
 }
