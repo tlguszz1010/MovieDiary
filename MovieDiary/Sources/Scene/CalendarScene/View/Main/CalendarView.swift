b6be8aab9f2a780686869d71412810bcb5ac363d
tlguszz1010 <tlguszz1010@naver.com>
2023-03-10 12:27:31 +0900

    FSCalendar 라이브러리 추가

---
+++ MovieDiary/Sources/Scene/CalendarScene/View/Main/CalendarView.swift
@@ -0,0 +1,21 @@
+//
+//  CalendarView.swift
+//  MovieDiary
+//
+//  Created by 박시현 on 2023/03/10.
+//
+
+import UIKit
+
+class CalendarView: BaseView {
+
+    
+    override func makeConfigures() {
+        
+    }
+    
+    override func makeConstraints() {
+        
+    }
+
+}
